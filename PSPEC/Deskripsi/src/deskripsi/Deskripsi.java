9db7c38bd209ea28d394d513fda914db7005e31f
yoannuramalia <54390583+yoannuramalia@users.noreply.github.com>
2019-09-13 21:08:03 +0700

    Add files via upload

---
+++ PSPEC/Deskripsi/src/deskripsi/Deskripsi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deskripsi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Yoan Nur Amalia
+ */
+public class Deskripsi {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner desbox = new Scanner(System.in);
+        System.out.print("Description Box : ");
+        desbox.nextLine();
+        System.out.println("Deskripsi anda berhasil diinput, silahkan pilih montir");
+
+    }
+}
