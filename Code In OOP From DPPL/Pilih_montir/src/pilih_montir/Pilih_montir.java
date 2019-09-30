88f228e73565914329675dc199f56dc871742058
farhanarrahmanp <54382334+farhanarrahmanp@users.noreply.github.com>
2019-09-30 08:29:46 +0700

    Add files via upload

---
+++ Code In OOP From DPPL/Pilih_montir/src/pilih_montir/Pilih_montir.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pilih_montir;
+
+/**
+ *
+ * @author Farhan
+ */
+public interface Pilih_montir {
+
+    public String getMontir();
+    
+    public void getCustomer();
+    
+    public void getAccept();
+    
+    public void getDescription();
+        
+}
